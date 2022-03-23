0b3e456985a50932e18dbfd7b3450785d432e0b1
Riccardo Perello <rperello@gio.lu>
2022-03-23 17:04:14 +0100

    structure html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 function createHtml(data) {
-    var iconsArray = data
+    var iconsObject = data
     var iconsContainer = document.getElementById("icons")
     var iconsHtml = ""
 
-    Object.keys(iconsArray).forEach(key => {
-        const obj = iconsArray[key]
-        iconsHtml += obj.code + "<br>" + "Name: " + obj.name + " <br>"
+    Object.keys(iconsObject).forEach(key => {
+        const obj = iconsObject[key]
+
+        let iconDiv = document.createElement("div")
+        iconDiv.append(obj.code)
+        iconDiv.classList.add('icon-container')
+
+        let nameDiv = document.createElement("div")
+        nameDiv.append(obj.name)
+        nameDiv.classList.add('label')
+
+        let itemDiv = document.createElement("div")
+        itemDiv.append(iconDiv + nameDiv)
+        itemDiv.classList.add('item')
+
+        iconsHtml += itemDiv
     })
 
     iconsContainer.innerHTML = iconsHtml
